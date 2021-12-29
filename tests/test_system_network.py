24126ba824d454fec1e9c7f6038fe6e9782423d1
Jan Schneider <j.schneider@uib.de>
2021-12-29 18:35:52 +0100

    [chg] Add test_system_network.py

---
+++ tests/test_system_network.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) uib GmbH <info@uib.de>
+# License: AGPL-3.0
+"""
+test_system_network
+"""
+
+from opsicommon.system.network import (
+	get_ip_addresses, get_fqdn, get_domain, get_hostnames
+)
+
+
+def test_get_ip_addresses():
+	addr = list(get_ip_addresses())
+	assert addr
+
+def test_get_fqdn():
+	assert get_fqdn()
+
+def test_get_domain():
+	assert get_domain()
+
+def test_get_hostnames():
+	hostnames = get_hostnames()
+	assert "localhost" in hostnames
