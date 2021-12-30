5403dda103bd331e9031d900a9aeba5e8c647b79
Jan Schneider <j.schneider@uib.de>
2021-12-30 20:06:59 +0100

    [chg] Add marker checks

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,8 +9,8 @@ This file is part of opsi - https://www.opsi.org
 """
 
 import os
+import platform
 import urllib3
-
 import pytest
 from _pytest.logging import LogCaptureHandler
 
@@ -23,6 +23,29 @@ def emit(*args, **kwargs) -> None:  # pylint: disable=unused-argument
 LogCaptureHandler.emit = emit
 
 
-@pytest.fixture
-def on_windows():
-	return bool(os.name == 'nt')
+def running_in_docker():
+	if not os.path.exists("/proc/self/cgroup"):
+		return False
+	with open("/proc/self/cgroup", "r", encoding="utf-8") as file:
+		for line in file.readlines():
+			if line.split(':')[2].startswith("/docker/"):
+				return True
+	return False
+
+PLATFORM = platform.system().lower()
+RUNNING_IN_DOCKER = running_in_docker()
+
+
+def pytest_runtest_setup(item):
+	supported_platforms = []
+	for marker in item.iter_markers():
+		if marker == "docker_linux" and not RUNNING_IN_DOCKER:
+			pytest.skip("Must run in docker")
+		if marker.name in ("windows", "linux", "darwin", "posix"):
+			if marker.name == "posix":
+				supported_platforms.extend(["linux", "darwin"])
+			else:
+				supported_platforms.append(marker.name)
+
+	if supported_platforms and PLATFORM not in supported_platforms:
+		pytest.skip(f"Cannot run on {PLATFORM}")
