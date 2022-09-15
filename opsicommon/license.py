092d88c3793242ac1598280c08172b1d2d202263
Jan Schneider <j.schneider@uib.de>
2022-09-15 20:38:11 +0200

    [fix] pylint

--- opsicommon/license.py
+++ opsicommon/license.py
@@ -266,8 +266,8 @@ class OpsiLicense:  # pylint: disable=too-few-public-methods,too-many-instance-a
 
 	_cached_signature_valid: Union[bool, None] = None
 
-	def __attrs_post_init__(self):
-		for attribute, value in attr.asdict(self).items():
+	def __attrs_post_init__(self) -> None:
+		for attribute, value in attr.asdict(self).items():  # pylint: disable=dotted-import-in-loop
 			if not attribute.startswith("_") and isinstance(value, str) and value.strip() == "":
 				setattr(self, attribute, None)
 
