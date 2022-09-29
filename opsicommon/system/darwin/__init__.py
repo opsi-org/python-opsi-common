0e05ef3ced5387beb6a83138a6e7a90de7b1a8a2
Jan Schneider <j.schneider@uib.de>
2022-09-29 14:26:57 +0000

    [chg] Return command output in Excetion

--- opsicommon/system/darwin/__init__.py
+++ opsicommon/system/darwin/__init__.py
@@ -11,6 +11,9 @@ from datetime import datetime
 
 
 def set_system_datetime(utc_datetime: datetime) -> None:
-	subprocess.run(
-		["date", "-f", "%Y-%m-%d %H:%M:%S %Z", "-u", utc_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")], capture_output=True, check=True
-	)
+	try:
+		subprocess.run(
+			["date", "-f", "%Y-%m-%d %H:%M:%S %Z", "-u", utc_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")], capture_output=True, check=True
+		)
+	except subprocess.CalledProcessError as err:
+		raise RuntimeError(f"Failed to set system time: {err.returncode} - {err.stderr.decode(errors='replace')}") from err
