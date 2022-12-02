675598cd37350c5794a957395e58a49acf1742ac
Jan Schneider <j.schneider@uib.de>
2022-12-02 14:49:39 +0000

    [chg] OpsiConfig and tests

--- opsicommon/config/opsi.py
+++ opsicommon/config/opsi.py
@@ -8,9 +8,11 @@ This file is part of opsi - https://www.opsi.org
 
 import re
 from pathlib import Path
+from threading import Lock
 from typing import Any
 
 from tomlkit import dumps, loads
+from tomlkit.items import Item
 
 from ..logging import get_logger
 from ..utils import Singleton
@@ -19,12 +21,60 @@ logger = get_logger("opsicommon.config")
 
 
 class OpsiConfig(metaclass=Singleton):
+	file_lock = Lock()
 	config_file = "/etc/opsi/opsi.conf"
+	default_config = {
+		"groups": {"fileadmingroup": "opsifileadmins", "admingroup": "opsiadmin"},
+		"packages": {"use_pigz": True},
+		"ldap_auth": {"ldap_url": "", "bind_user": ""},
+	}
 
 	def __init__(self) -> None:
-		self._config: dict[str, Any] = {}
+		self._config_file_mtime = 0.0
+		self._config: dict[str, Any] = self.default_config
+		self._config_file_read = False
 
-	def update_config_file(self) -> None:
+	@staticmethod
+	def _merge_config(destination: dict[str, Any], source: dict[str, Any]) -> None:
+		for key in source:
+			if key not in destination:
+				# Do not create new configs / categories
+				continue
+			if isinstance(source[key], dict):
+				OpsiConfig._merge_config(destination[key], source[key])
+			else:
+				destination[key] = source[key]
+
+	def _assert_config_read(self) -> None:
+		if not self._config_file_read or Path(self.config_file).stat().st_mtime != self._config_file_mtime:
+			self.read_config_file()
+
+	def _assert_category_and_config(self, category: str, config: str) -> None:
+		if category not in self._config:
+			raise ValueError(f"Invalid category {category!r}")
+		if config not in self._config[category]:
+			raise ValueError(f"Invalid config {config!r} for category {category!r}", config, category)
+
+	def get(self, category: str, config: str) -> Any:
+		self._assert_config_read()
+		self._assert_category_and_config(category, config)
+		return self._config[category][config]
+
+	def set(self, category: str, config: str, value: Any, persistent: bool = False) -> None:
+		self._assert_config_read()
+		self._assert_category_and_config(category, config)
+		value = "" if value is None else value
+		_type = type(self._config[category][config])
+		if isinstance(self._config[category][config], Item):
+			_type = type(self._config[category][config].unwrap())
+		if not isinstance(value, _type):
+			raise ValueError(f"Wrong type {type(value).__name__!r} for config {config!r} ({_type.__name__}) in category {category!r}")
+		self._config[category][config] = value
+		if persistent:
+			self.write_config_file()
+
+	def upgrade_config_file(self) -> None:
+		# Convert ini (opsi < 4.3) to toml (opsi >= 4.3)
 		file = Path(self.config_file)
 		data = file.read_text(encoding="utf-8")
 		key_val_regex = re.compile(r"([^=]+)=(\s*)(.+)")
@@ -44,9 +94,17 @@ class OpsiConfig(metaclass=Singleton):
 			file.write_text(new_data, encoding="utf-8")
 
 	def read_config_file(self) -> None:
-		self.update_config_file()
-		data = Path(self.config_file).read_text(encoding="utf-8")
-		self._config = loads(data)
+		with self.file_lock:
+			self._config_file_read = False
+			self.upgrade_config_file()
+			file = Path(self.config_file)
+			data = file.read_text(encoding="utf-8")
+			self._merge_config(self._config, loads(data))
+			self._config_file_read = True
+			self._config_file_mtime = file.stat().st_mtime
 
 	def write_config_file(self) -> None:
-		Path(self.config_file).write_text(dumps(self._config), encoding="utf-8")
+		with self.file_lock:
+			file = Path(self.config_file)
+			file.write_text(dumps(self._config), encoding="utf-8")
+			self._config_file_mtime = file.stat().st_mtime
--- tests/test_config.py
+++ tests/test_config.py
@@ -7,11 +7,13 @@ This file is part of opsi - https://www.opsi.org
 from pathlib import Path
 from textwrap import dedent
 
+import pytest
 from opsicommon.config import OpsiConfig
 
 
-def test_update_config_from_ini(tmp_path: Path) -> None:
+def test_upgrade_config_from_ini(tmp_path: Path) -> None:
 	config_file = tmp_path / "opsi.conf"
+	OpsiConfig._instances = {}  # pylint: disable=protected-access
 	OpsiConfig.config_file = str(config_file)
 	data = """
 	[groups]
@@ -28,7 +30,7 @@ def test_update_config_from_ini(tmp_path: Path) -> None:
 	data = dedent(data)
 	config_file.write_text(data, encoding="utf-8")
 	config = OpsiConfig()
-	config.update_config_file()
+	config.upgrade_config_file()
 	new_data = config_file.read_text(encoding="utf-8")
 	assert new_data == dedent(
 		"""
@@ -44,3 +46,78 @@ def test_update_config_from_ini(tmp_path: Path) -> None:
 	ldap_url = "ldaps://ad.opsi.test/dc=ad,dc=opsi,dc=test"
 	"""
 	)
+
+
+def test_read_config_file(tmp_path: Path) -> None:
+	config_file = tmp_path / "opsi.conf"
+	OpsiConfig._instances = {}  # pylint: disable=protected-access
+	OpsiConfig.config_file = str(config_file)
+	data = """
+	[ldap_auth]
+	ldap_url = "ldaps://test"
+	"""
+	config_file.write_text(dedent(data), encoding="utf-8")
+	config = OpsiConfig()
+	assert config._config_file_mtime == 0.0  # pylint: disable=protected-access
+	assert config.get("ldap_auth", "ldap_url") == "ldaps://test"
+	mtime = config._config_file_mtime  # pylint: disable=protected-access
+	assert mtime != 0.0
+
+
+def test_get_config(tmp_path: Path) -> None:
+	config_file = tmp_path / "opsi.conf"
+	OpsiConfig._instances = {}  # pylint: disable=protected-access
+	OpsiConfig.config_file = str(config_file)
+	data = """
+	[groups]
+	fileadmingroup = "fag"
+	"""
+	config_file.write_text(dedent(data), encoding="utf-8")
+	config = OpsiConfig()
+	assert config.get("groups", "fileadmingroup") == "fag"
+
+
+def test_set_config(tmp_path: Path) -> None:
+	config_file = tmp_path / "opsi.conf"
+	OpsiConfig._instances = {}  # pylint: disable=protected-access
+	OpsiConfig.config_file = str(config_file)
+	data = """
+	[groups]
+	fileadmingroup = "fag"
+	admingroup = "ag"
+	"""
+	config_file.write_text(dedent(data), encoding="utf-8")
+	config = OpsiConfig()
+	config.set("groups", "fileadmingroup", "new", persistent=True)
+	new_data = config_file.read_text(encoding="utf-8")
+	assert (
+		dedent(
+			"""
+	[groups]
+	fileadmingroup = "new"
+	admingroup = "ag"
+	"""
+		).strip()
+		in new_data
+	)
+
+
+def test_set_config_type_check() -> None:
+	config = OpsiConfig()
+	with pytest.raises(ValueError, match=r"Wrong type 'str' for config 'use_pigz' \(bool\) in category 'packages'"):
+		config.set("packages", "use_pigz", "yes")
+
+	with pytest.raises(ValueError, match=r"Wrong type 'bool' for config 'fileadmingroup' \(str\) in category 'groups'"):
+		config.set("groups", "fileadmingroup", True)
+
+	with pytest.raises(ValueError, match=r"Wrong type 'int' for config 'ldap_url' \(str\) in category 'ldap_auth'"):
+		config.set("ldap_auth", "ldap_url", 123)
+
+
+def test_set_config_invalid_category_or_config() -> None:
+	config = OpsiConfig()
+	with pytest.raises(ValueError, match=r"Invalid config 'invalid' for category 'packages'"):
+		config.set("packages", "invalid", True)
+
+	with pytest.raises(ValueError, match=r"Invalid category 'invalid'"):
+		config.set("invalid", "invalid", True)
