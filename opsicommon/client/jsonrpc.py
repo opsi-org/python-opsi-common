b1b989074e15dc64837410ec94e90e852078b4e7
Jan Schneider <j.schneider@uib.de>
2023-03-28 08:16:49 +0000

    [chg,pub] Timeout 1800 for depot_createMd5SumFile and depot_createZsyncFile

--- opsicommon/client/jsonrpc.py
+++ opsicommon/client/jsonrpc.py
@@ -80,6 +80,8 @@ class JSONRPCClient:  # pylint: disable=too-many-instance-attributes
 		"depot_installPackage": 3600,
 		"depot_librsyncPatchFile": 24 * 3600,
 		"depot_getMD5Sum": 3600,
+		"depot_createMd5SumFile": 1800,
+		"depot_createZsyncFile": 1800,
 	}
 	no_proxy_addresses = ["localhost", "127.0.0.1", "ip6-localhost", "::1"]  # pylint: disable=use-tuple-over-list
 
