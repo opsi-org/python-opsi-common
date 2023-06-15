6a392a9338698602afb91ef4cfd7be73524e4515
Jan Schneider <j.schneider@uib.de>
2023-06-15 20:51:04 +0000

    [chg] Set new limits

--- tests/test_package_archive.py
+++ tests/test_package_archive.py
@@ -112,17 +112,17 @@ def test_archive_internal(tmp_path: Path, compression: Literal["zstd", "bz2", "g
 	"mode, compression, expect_min_percent_same",
 	(
 		# external
-		("external", None, 88),
+		("external", None, 87),
 		("external", "zstd", 87),
 		("external", "bz2", 0),
 		("external", "gz", 74),
 		# internal
-		("internal", None, 88),
+		("internal", None, 87),
 		("internal", "zstd", 87),
 		("internal", "bz2", 0),
-		("internal", "gz", 64),
+		("internal", "gz", 60),
 		# auto
-		("auto", None, 88),
+		("auto", None, 87),
 		("auto", "zstd", 87),
 		("auto", "bz2", 0),
 		("auto", "gz", 74),
