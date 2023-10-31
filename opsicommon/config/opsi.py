6eda256cda4d354b31803d45c739a9828f6fa1d2
Fabian Kalweit <f.kalweit@uib.de>
2023-10-31 16:33:24 +0100

    [chg] if run as user

--- opsicommon/config/opsi.py
+++ opsicommon/config/opsi.py
@@ -50,7 +50,7 @@ def get_opsiconfd_user() -> str:
 			line = line.strip()
 			if not line or line.startswith("#") or "=" not in line:
 				continue
-			if "run-as-user" in line.split("=", 1)[0]:
+			if "run-as-user" == line.split("=", 1)[0].strip():
 				return line.split("=", 1)[1].strip()
 	return DEFAULT_OPSICONFD_USER
 
