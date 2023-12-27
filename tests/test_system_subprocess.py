7bd1ff8ef2e022b4c98bf729f9071626b539b8ec
Jan Schneider <j.schneider@uib.de>
2023-12-27 09:56:48 +0000

    [chg] Sleep 2 seconds

--- tests/test_system_subprocess.py
+++ tests/test_system_subprocess.py
@@ -40,6 +40,7 @@ def test_ld_library_path() -> None:
 def test_path_cleanup() -> None:
 	path = ["/usr/local/sbin", "", "/usr/local/bin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "", "/bin"]
 	clean_path = ["/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"]
+	cmd = "sleep 2"
 	if os.name == "nt":
 		path = [
 			r"C:\Program Files (x86)\Python311-32\Scripts",
@@ -66,10 +67,11 @@ def test_path_cleanup() -> None:
 			r"C:\Program Files (x86)\Git\cmd",
 			r"C:\Program Files (x86)\opsi.org\opsi-client-agent\opsiclientd_bin\pywin32_system32",
 		]
+		cmd = "timeout 2"
 	patch_popen()
 	with environment(PATH=os.pathsep.join(path)):
 		assert os.environ["PATH"].split(os.pathsep) == path
-		with subprocess.Popen("echo opsi", shell=True) as proc:
+		with subprocess.Popen(cmd, shell=True) as proc:
 			ps_proc = psutil.Process(proc.pid)
 			proc_env = ps_proc.environ()
 			assert proc_env["PATH"].split(os.pathsep) == clean_path
