d05a987a9a7cd3d7c6a73730609a5d1912ef0ad7
Jan Schneider <j.schneider@uib.de>
2024-11-19 12:42:45 +0100

    [chg,pub] Add new modules to license.py

--- opsicommon/license.py
+++ opsicommon/license.py
@@ -73,9 +73,11 @@ OPSI_MODULE_STATE_OVER_LIMIT = "over_limit"
 OPSI_MODULE_STATE_CLOSE_TO_LIMIT = "close_to_limit"
 
 OPSI_MODULE_IDS = (
+	"custom_ca",
 	"directory-connector",
 	"dynamic_depot",
 	"install_by_shutdown",
+	"letsencrypt",
 	"license_management",
 	"linux_agent",
 	"local_imaging",
@@ -85,6 +87,7 @@ OPSI_MODULE_IDS = (
 	"roaming_profiles",
 	"scalability1",
 	"secureboot",
+	"sso",
 	"swondemand",
 	"treeview",
 	"uefi",
